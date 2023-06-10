41a347669b2fab416836c8ee6bb39f71bc1262d2
Dishant Nagpal <dishantnagpal10@gmail.com>
2023-06-10 21:36:53 +0530

    Added MapView

---
+++ DM_Companion/DM_Companion/Dishant_files/MapView.swift
@@ -0,0 +1,19 @@
+//
+//  MapView.swift
+//  DM_Companion
+//
+//  Created by Dishant Nagpal on 10/06/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map()
+    }
+}
+
+#Preview {
+    MapView()
+}
